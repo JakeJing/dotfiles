c9d92184cedc762616386efc654448215a5c9ac2
JakeJing <350956009@qq.com>
2023-06-08 10:47:39 +0200

    edit st.fish desc

--- .config/fish/functions/st.fish
+++ .config/fish/functions/st.fish
@@ -1,4 +1,4 @@
-function st --description "syncronize the dot files"
+function st --description "syncronize the dotfiles and update the git repos"
     cd ~/dotfiles && stow --adopt -d . -t ~ .
     gu $argv
 end
